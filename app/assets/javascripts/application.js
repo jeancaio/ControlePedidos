06898e2e5d6e80103735ed8c14194fab59071599
jeancaio <jeanandreolla@hotmail.com>
2017-09-04 08:40:40 -0300

    corrigindo bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery/dist/jquery.min.js
 //= require jquery.inputmask
 //= require jquery.inputmask.extensions
 //= require jquery.inputmask.numeric.extensions
